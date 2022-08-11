12ba8c83322d6016ce6d33e48c01b7bad8fa3d2d
levyd01 <levyd01.addons@gmail.com>
2022-08-11 10:45:58 +0200

    Created scripts for test automation and added two tests

---
+++ tb/tb_decoder/golden/C_model_to_C_golden.py
@@ -0,0 +1,48 @@
+import os
+import argparse
+
+# Parse input parameters
+parser  = argparse.ArgumentParser(description="Get test number to process", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+requiredNamed = parser.add_argument_group('required named arguments')
+requiredNamed.add_argument('-t', '--test_number', help='Test number to process', required=True)
+args = parser.parse_args()
+config = vars(args)
+test_nbr = config['test_number']
+print("Processing test " + str(test_nbr))
+
+# Read config file
+with open('test' + str(test_nbr) + '/test_cfg.txt') as fr:
+    line = fr.readline() # Number of slices per line
+    line_split = line.split('#')
+    slicesPerLine = int(line_split[0])
+    line = fr.readline()  # bpp (only used by verilog testbench)
+    line = fr.readline()  # Source image name
+    line_split = line.split('#')
+    inFile = line_split[0].rstrip()
+    line = fr.readline()  # VDCM version (1.1 and 1.2 are the only valid values)
+    line_split = line.split('#')
+    vdcm_version = line_split[0].rstrip()
+    line = fr.readline()  # Configuration file name
+    line_split = line.split('#')
+    configFile = line_split[0].rstrip()
+    line = fr.readline()  # Slice Height override
+    line_split = line.split('#')
+    sliceHeight = int(line_split[0])
+    
+
+os.chdir("./../../../c_model/x64")
+# Run VDCM Encoder to generate compressed vdcm.bits
+inFileArg = "-inFile ../../images/" + inFile
+bitstreamArg = "-bitstream vdcm.bits"
+configFileArg = "-configFile ../config_files/v" + vdcm_version + "/" + configFile
+sliceHeightArg = ""
+if (sliceHeight != 0): # Override default slice height
+   sliceHeightArg = "-sliceHeight " + sliceHeight
+os.system("VDCM_Encoder.exe " + inFileArg + " " + bitstreamArg + " " + configFileArg + " " + sliceHeightArg)
+# Run VDCM Decoder to generate golden image
+recFileArg = "-recFile golden_image.out.ppm"
+os.system("VDCM_Decoder.exe " + bitstreamArg + " " + recFileArg + " -debugTracer")
+
+
+
+os.chdir("./../../tb/tb_decoder/golden")
