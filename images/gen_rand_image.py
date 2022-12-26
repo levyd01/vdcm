8cd9ef7239bae694b04b65fd7e82eb1c2a52cfb4
levyd01 <levyd01.addons@gmail.com>
2022-12-26 15:17:45 +0100

    Added new images and tests to improve coverage.

--- images/gen_rand_image.py
+++ images/gen_rand_image.py
@@ -8,9 +8,18 @@ class ImgFormat(Enum):
    
     def __str__(self):
         return self.value
+        
+class ImgType(Enum):
+    rand = "rand"
+    black = "black"
+   
+    def __str__(self):
+        return self.value
+
    
 # Parse input parameters
-parser  = argparse.ArgumentParser(description="Parse test generation parameters", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser  = argparse.ArgumentParser(description="Parse image generation parameters", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-t", "--img_type", type=ImgType, choices=list(ImgType), default=ImgType.rand, help="Image format: rand or black")
 parser.add_argument("-f", "--img_format", type=ImgFormat, choices=list(ImgFormat), default=ImgFormat.ppm, help="Image format: ppm or ycocg")
 parser.add_argument("-x", '--img_width', type=int, default=640, help="Image width")
 parser.add_argument("-y", '--img_height', type=int, default=480, help="Image height")
@@ -39,8 +48,13 @@ if (args.img_format == ImgFormat.ppm):
        for py in range(h):
           for px in range(w):
              for pc in range(3):
-                 rand_int = random.randint(0, max_value)
-                 rand_bytes = rand_int.to_bytes(num_bytes, 'big')
-                 binary_file.write(rand_bytes)
+                 if (args.img_type == ImgType.rand):
+                    rand_int = random.randint(0, max_value)
+                    rand_bytes = rand_int.to_bytes(num_bytes, 'big')
+                    binary_file.write(rand_bytes)
+                 else:
+                    black_int = 0
+                    black_bytes = black_int.to_bytes(num_bytes, 'big')
+                    binary_file.write(black_bytes)
 else:
    print("image format ycocg is TBD" )
